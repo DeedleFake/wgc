e3930dd38d76765f1382be06bac5623d8dc68a00
DeedleFake <deedlefake@users.noreply.github.com>
2016-03-08 14:33:21 -0500

    Output words in decreasing length order.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod words;
 
 use std::env;
+use std::collections::{BTreeSet};
 
 fn matches(pat: &String, line: &str) -> bool {
     fn check<P, L>(mut pc: P, lc: L) -> bool where P: Iterator<Item=char>, L: Iterator<Item=char> {
@@ -10,6 +11,9 @@ fn matches(pat: &String, line: &str) -> bool {
                 Some(ch) => {
                     if outer == ch {
                         c = pc.next();
+                        if c.is_none() {
+                            return true;
+                        }
                     }
                     else if words::consonant(outer) {
                         return false;
@@ -33,6 +37,15 @@ fn usage(args: Vec<String>) -> ! {
     std::process::exit(2);
 }
 
+#[derive(Eq, PartialEq, PartialOrd)]
+struct Word(String);
+
+impl Ord for Word {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.len().cmp(&other.0.len()).reverse()
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -48,12 +61,16 @@ fn main() {
         matches(&args[1], line)
     });
 
-    let mut found = false;
+    let mut found = BTreeSet::new();
     for word in words {
-        println!("{}", word);
-        found = true;
+        found.insert(Word(word));
     }
-    if !found {
+    if found.len() == 0 {
         println!("No matches found.");
+        return;
+    }
+
+    for word in found.iter() {
+        println!("{}", word.0);
     }
 }
