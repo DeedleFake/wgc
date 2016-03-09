b832befc77b2030735f692bc7decedd35a041560
DeedleFake <deedlefake@users.noreply.github.com>
2016-03-09 14:40:59 -0500

    Remove now-unused HeapDrain.

--- src/sort.rs
+++ src/sort.rs
@@ -1,5 +1,4 @@
 use std::cmp::{Ordering};
-use std::collections::{BinaryHeap};
 
 #[derive(Eq, PartialEq, Debug)]
 pub struct Length(pub String);
@@ -22,30 +21,6 @@ impl PartialOrd for Length {
     }
 }
 
-pub struct HeapDrain<'a, T> where T: 'a + Ord {
-    heap: &'a mut BinaryHeap<T>,
-}
-
-pub trait HeapPopDrain<T> where T: Ord {
-    fn pop_drain(&mut self) -> HeapDrain<T>;
-}
-
-impl<T> HeapPopDrain<T> for BinaryHeap<T> where T: Ord {
-    fn pop_drain(&mut self) -> HeapDrain<T> {
-        HeapDrain{
-            heap: self,
-        }
-    }
-}
-
-impl<'a, T> Iterator for HeapDrain<'a, T> where T: 'a + Ord {
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.heap.pop()
-    }
-}
-
 #[cfg(test)]
 mod test {
     #[test]
@@ -60,37 +35,5 @@ mod test {
         assert_eq!(short.cmp(&longer), Ordering::Less);
         assert_eq!(longer.cmp(&short), Ordering::Greater);
         assert_eq!(short.cmp(&equal), Ordering::Equal);
-    }
-
-    #[test]
-    fn test_heapdrain() {
-        use super::{Length, HeapPopDrain};
-        use std::collections::{BinaryHeap};
-
-        {
-            let mut heap = BinaryHeap::new();
-            heap.push(3);
-            heap.push(9);
-            heap.push(6);
-
-            let mut drain = heap.pop_drain();
-            assert_eq!(drain.next(), Some(9));
-            assert_eq!(drain.next(), Some(6));
-            assert_eq!(drain.next(), Some(3));
-            assert_eq!(drain.next(), None);
-        }
-
-        {
-            let mut heap = BinaryHeap::new();
-            heap.push(Length("short".to_string()));
-            heap.push(Length("notequal".to_string()));
-            heap.push(Length("longer".to_string()));
-
-            let mut drain = heap.pop_drain();
-            assert_eq!(drain.next(), Some(Length("notequal".to_string())));
-            assert_eq!(drain.next(), Some(Length("longer".to_string())));
-            assert_eq!(drain.next(), Some(Length("short".to_string())));
-            assert_eq!(drain.next(), None);
-        }
     }
 }
